e79df553863db124d7664a8ba4e51a500828c425
haoyan.zhang <haoyan.zhang@qunar.com>
2018-02-25 10:34:55 +0800

    2

--- src/main/java/com/java/web/template/controller/HealthCheckController.java
+++ src/main/java/com/java/web/template/controller/HealthCheckController.java
@@ -1,13 +1,16 @@
 package com.java.web.template.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HealthCheckController {
 
     @RequestMapping("healthCheck")
     public String healthCheck(){
+        log.info("check");
         return "Check";
     }
 }
