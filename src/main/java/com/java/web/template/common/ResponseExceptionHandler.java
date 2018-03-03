2c1dbbc02123440fdc243542ddf2b31a07aea69f
haoyan.zhang <haoyan.zhang@qunar.com>
2018-03-03 18:41:28 +0800

    5

--- src/main/java/com/java/web/template/common/ResponseExceptionHandler.java
+++ src/main/java/com/java/web/template/common/ResponseExceptionHandler.java
@@ -4,16 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Slf4j
 @ControllerAdvice
-public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
+public class ResponseExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public JsonResult handleAllException(Exception e) {
         return JsonResult.fail(e.getMessage());
     }
-
 }
