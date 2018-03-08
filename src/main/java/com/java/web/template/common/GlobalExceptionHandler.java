b2c4e6f3ae4ce0d24d742d1f309fcd68355b93b1
haoyan.zhang <haoyan.zhang@qunar.com>
2018-03-08 22:51:04 +0800

    5

--- src/main/java/com/java/web/template/common/GlobalExceptionHandler.java
+++ src/main/java/com/java/web/template/common/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.java.web.template.common;
 
+import com.java.web.template.exception.BusinessException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +13,12 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public JsonResult handleAllException(Exception e) {
+        if(e instanceof BusinessException){
+
+        }
+        else{
+            log.error("",e);
+        }
         return JsonResult.fail(e.getMessage());
     }
 }
